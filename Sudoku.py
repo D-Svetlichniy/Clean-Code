c3d9aaf28926f3b3dff598db4db04075a9342a65
Dymit <svetlichniy.dimas@gmail.com>
2023-05-14 12:35:53 +0300

    Project after refactor

--- Sudoku.py
+++ Sudoku.py
@@ -3,26 +3,21 @@ import numpy as np
 import random
 
 
-def generate_sudoku(mask_rate):
-    while True:
-        n = 9
-        solution = np.zeros((n, n), np.int32)
-        rg = np.arange(1, n + 1)
-        solution[0, :] = np.random.choice(rg, n, replace=False)
-        try:
-            for r in range(1, n):
-                for c in range(n):
-                    col_rest = np.setdiff1d(rg, solution[:r, c])
-                    row_rest = np.setdiff1d(rg, solution[r, :c])
-                    avb1 = np.intersect1d(col_rest, row_rest)
-                    sub_r, sub_c = r // 3, c // 3
-                    avb2 = np.setdiff1d(np.arange(0, n + 1),
-                                        solution[sub_r * 3:(sub_r + 1) * 3, sub_c * 3:(sub_c + 1) * 3].ravel())
-                    avb = np.intersect1d(avb1, avb2)
-                    solution[r, c] = np.random.choice(avb, size=1)
-            break
-        except ValueError:
-            pass
+def generate_sudoku(mask_rate, n):
+    solution = np.zeros((n, n), np.int32)
+    rg = np.arange(1, n + 1)
+    solution[0, :] = np.random.choice(rg, n, replace=False)
+    for r in range(1, n):
+        for c in range(n):
+            col_rest = np.setdiff1d(rg, solution[:r, c])
+            row_rest = np.setdiff1d(rg, solution[r, :c])
+            available_numbers_col = np.intersect1d(col_rest, row_rest)
+            subgrid_row, subgrid_col = r // 3, c // 3
+            available_numbers_subgrid = np.setdiff1d(np.arange(0, n + 1),
+                                                     solution[subgrid_row * 3:(subgrid_row + 1) * 3,
+                                                     subgrid_col * 3:(subgrid_col + 1) * 3].ravel())
+            available_numbers = np.intersect1d(available_numbers_col, available_numbers_subgrid)
+            solution[r, c] = np.random.choice(available_numbers, size=1)
     puzzle = solution.copy()
     puzzle[np.random.choice([True, False], size=solution.shape, p=[mask_rate, 1 - mask_rate])] = 0
     return puzzle, solution
@@ -34,29 +29,25 @@ def check_progress(window, solution):
         for c, col in enumerate(row):
             value = window[r, c].get()
             if value:
-                try:
-                    value = int(value)
-                except:
-                    value = 0
-                if value != solution[r][c]:
-                    window[r, c].update(background_color='red')
-                    solved = False
-                else:
-                    window[r, c].update(background_color=sg.theme_input_background_color())
-            else:
+                value = int(value)
+            if value != solution[r][c]:
+                window[r, c].update(background_color='red')
                 solved = False
-                window[r, c].update(background_color='yellow')
+            else:
+                window[r, c].update(background_color=sg.theme_input_background_color())
+        else:
+            solved = False
+            window[r, c].update(background_color='yellow')
     return solved
 
 
-def create_and_show_puzzle(window):
-    rate = DEFAULT_MASK_RATE
-    if window['-RATE-'].get():
+def create_and_show_puzzle(window, rate_flag, mask_rate, n):
+    if window[rate_flag].get():
         try:
-            rate = float(window['-RATE-'].get())
+            mask_rate = float(window[rate_flag].get())
         except:
             pass
-    puzzle, solution = generate_sudoku(mask_rate=rate)
+    puzzle, solution = generate_sudoku(mask_rate, n)
     for r, row in enumerate(puzzle):
         for c, col in enumerate(row):
             window[r, c].update(puzzle[r][c] if puzzle[r][c] else '',
@@ -64,13 +55,23 @@ def create_and_show_puzzle(window):
     return puzzle, solution
 
 
-def main(mask_rate=0.1):
-    window = sg.Window('Sudoku',
-                       [[sg.Frame('', [[sg.I(random.randint(1, 9), justification='r', size=(3, 1), enable_events=True,
-                                             key=(fr * 3 + r, fc * 3 + c)) for c in range(3)] for r in range(3)]) for fc
-                         in range(3)] for fr in range(3)] +
-                       [[sg.B('Solve'), sg.B('Check'), sg.B('Hint'), sg.B('New Game'), sg.T('Mask rate (0-1)'),
-                         sg.In(str(mask_rate), size=(3, 1), key='-RATE-')], ], finalize=True)
+def main(mask_rate, n, rate_flag, grid_size, solve_flag, check_flag, hint_flag, new_game_flag, mask_rate_flag):
+    window_layout = [
+        [sg.Frame('', [
+            [sg.I(random.randint(1, 9), justification='r', size=(grid_size, 1), enable_events=True,
+                  key=(fr * grid_size + r, fc * grid_size + c)) for c in range(grid_size)]
+            for r in range(grid_size)])
+         for fc in range(grid_size)]
+        for fr in range(grid_size)],[
+                sg.B(solve_flag),
+                sg.B(check_flag),
+                sg.B(hint_flag),
+                sg.B(new_game_flag),
+                sg.T(mask_rate_flag),
+                sg.In(str(mask_rate), size=(grid_size, 1), key=rate_flag)
+            ]
+
+    window = sg.Window('Sudoku', window_layout, finalize=True)
 
     puzzle, solution = create_and_show_puzzle(window)
     check_showing = False
@@ -79,23 +80,23 @@ def main(mask_rate=0.1):
         if event == sg.WIN_CLOSED:
             break
 
-        if event == 'Solve':
+        if event == solve_flag:
             for r, row in enumerate(solution):
                 for c, col in enumerate(row):
                     window[r, c].update(solution[r][c], background_color=sg.theme_input_background_color())
-        elif event == 'Check':
+        elif event == check_flag:
             check_showing = True
             solved = check_progress(window, solution)
             if solved:
                 sg.popup('Solved! You have solved the puzzle correctly.')
-        elif event == 'Hint':
+        elif event == hint_flag:
             elem = window.find_element_with_focus()
             try:
                 elem.update(solution[elem.Key[0]][elem.Key[1]], background_color=sg.theme_input_background_color())
             except:
                 pass
-        elif event == 'New Game':
-            puzzle, solution = create_and_show_puzzle(window)
+        elif event == new_game_flag:
+            puzzle, solution = create_and_show_puzzle(window, rate_flag, mask_rate, n)
         elif check_showing:
             check_showing = False
             for r, row in enumerate(solution):
@@ -106,4 +107,12 @@ def main(mask_rate=0.1):
 
 if __name__ == "__main__":
     DEFAULT_MASK_RATE = 0.1
-    main(DEFAULT_MASK_RATE)
+    n = 9
+    MASK_RATE_FLAG = 'Mask rate (0-1)'
+    RATE_FLAG = '-RATE-'
+    GRID_SIZE = 3
+    SOLVE_FLAG = 'Solve'
+    CHECK_FLAG = 'Check'
+    HINT_FLAG = 'Hint'
+    NEW_GAME_FLAG = 'New game'
+    main(DEFAULT_MASK_RATE, n, RATE_FLAG, GRID_SIZE, SOLVE_FLAG, CHECK_FLAG, HINT_FLAG, NEW_GAME_FLAG, MASK_RATE_FLAG)
